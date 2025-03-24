f073185a880b8174dce589aba9bb9957a019bf64
animallibraryn4 <nikhil7858978052@gmail.com>
2025-03-24 23:59:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from pyrogram.enums import ParseMode
 import sys
 from pyromod import listen
 from datetime import datetime
-from plugins import flink
 
 from config import ADMINS, API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL,FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT, OWNER_ID
 
